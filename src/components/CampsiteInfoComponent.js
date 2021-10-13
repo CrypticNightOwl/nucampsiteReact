069877f4c24c252241539f49a67e0a194f268cd4
ErickG <erick818@gmail.com>
2021-10-13 15:41:40 -0700

    Presentational and Container Components.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -45,17 +45,16 @@ class CampsiteInfo extends Component {
 
         if (this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             )
         } else {
             return (<div></div>)
         }
-
-
-
     }
 }
 
